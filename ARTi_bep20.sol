c13fdab804857293a9d70a01a8a5194809b64e96
ARTi <84725542+artiproject@users.noreply.github.com>
2021-12-26 16:58:10 +0900

    Update ARTi_bep20.sol

--- ARTi_bep20.sol
+++ ARTi_bep20.sol
@@ -1,5 +1,7 @@
 /**
 * ARTi Token - Smart Contract for Binance Smart Chain  
+ 0x5d33254591f34c89385AF385A8cC8a16A1326EB1
+ https://bscscan.com/address/0x5d33254591f34c89385AF385A8cC8a16A1326EB1#contracts
 */
 
 
